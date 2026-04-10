22653bdf0e1bdd13213f60ba35cbd4ed3e1a013e
Yarlei Cruz <yarleicruz1998@gmail.com>
2026-04-10 11:55:12 -0300

    delete: remove legacy LandingPage, Navbar, and SearchOrderPage classes as part of project cleanup

--- playwright/backup/legacy/Navbar.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test'
-
-export class Navbar {
-    constructor(private page: Page) { }
-
-    async searchOrderLink() {
-        await this.page.getByRole('link', { name: 'Consultar Pedido' }).click()
-    }
-}
